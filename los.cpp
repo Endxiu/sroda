117f582bfb9cf2b3d60a84ab3305e1088bc0f36b
Endxiu <xminion1908@gmail.com>
2024-02-11 15:06:49 +0100

    Sortowanie przez wstrząsanie/koktajlowe

--- los.cpp
+++ los.cpp
@@ -17,15 +17,37 @@ void wyswietl(int ROZMIAR, int wyl_liczby[]){
 }
  
 void sort_bombel(int tab[], int ROZMIAR){
-    for(int i=0;i<=ROZMIAR;i++){
-		for(int j=1;j<=ROZMIAR-i;j++){
-		    if(tab[j-1]>tab[j]){
-		        swap(tab[j-1] , tab[j]);
+    for(int i = 0; i <= ROZMIAR; i++){
+		for(int j = 1; j <= ROZMIAR - i; j++){
+		    if(tab[j - 1] > tab[j]){
+		        swap(tab[j - 1], tab[j]);
 		    }
 		}
     }
 }
- 
+
+void sort_wstrzas(int tab[], int ROZMIAR){
+  int left = 0, right = ROZMIAR - 1;
+  bool zamiana = true;
+  while (zamiana == true){
+    zamiana = false;
+    for (int i = left; i < right; i++){
+      if (tab[i] > tab[i + 1]){
+        swap(tab[i], tab[i + 1]);
+        zamiana = true;
+      }
+    }
+    right = right - 1;
+    for (int i = right; i > left; i--){
+      if (tab[i] < tab[i - 1]){
+        swap(tab[i], tab[i - 1]);
+        zamiana = true;
+      }
+    }
+    left = left + 1;
+  }
+}
+
 int main()
 {
     constexpr int ROZMIAR = 12;
@@ -34,5 +56,8 @@ int main()
     wyswietl(ROZMIAR,tab);
     cout<<endl;
     sort_bombel(tab,ROZMIAR);
+    wyswietl(ROZMIAR,tab);
+    cout<<endl;
+    sort_wstrzas(tab,ROZMIAR);
     wyswietl(ROZMIAR,tab);
 }
