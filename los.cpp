e3a857ec2df861cf624f00ffd8765abf506b1308
Endxiu <xminion1908@gmail.com>
2024-02-11 14:43:56 +0100

    Sortowanie Bombelkowe

--- los.cpp
+++ los.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-
+ 
+void wygeneruj(int ROZMIAR, int wyl_liczby[]){
+    srand((unsigned) time(NULL));
+    for(int i = 0; i <= ROZMIAR; i++){
+        int random = rand() % 25 + 1;
+        wyl_liczby[i] = random;
+    }
+}
+ 
+void wyswietl(int ROZMIAR, int wyl_liczby[]){
+    for(int i = 0; i <= ROZMIAR; i++){
+    cout<<wyl_liczby[i]<<" ";
+    }
+}
+ 
+void sort_bombel(int tab[], int ROZMIAR){
+    for(int i=0;i<=ROZMIAR;i++){
+		for(int j=1;j<=ROZMIAR-i;j++){
+		    if(tab[j-1]>tab[j]){
+		        swap(tab[j-1] , tab[j]);
+		    }
+		}
+    }
+}
+ 
 int main()
 {
     constexpr int ROZMIAR = 12;
-    int wyl_liczby[ROZMIAR];
-    for(int i = 0; i < ROZMIAR; i++){
-        int random = rand() % 25 + 1;
-        wyl_liczby[i] = random;
-        cout<<wyl_liczby[i]<<" ";
-    }
+    int tab[ROZMIAR];
+    wygeneruj(ROZMIAR,tab);
+    wyswietl(ROZMIAR,tab);
+    cout<<endl;
+    sort_bombel(tab,ROZMIAR);
+    wyswietl(ROZMIAR,tab);
 }
